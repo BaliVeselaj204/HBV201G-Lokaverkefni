f573ed19144754935835e89e866db6bfa1d40f58
Jóhann <jgf1@hi.is>
2026-03-31 14:04:10 +0000

    Added 2 recipies to the original list

--- src/main/java/is/vinnsla/RecipeManager.java
+++ src/main/java/is/vinnsla/RecipeManager.java
@@ -9,8 +9,13 @@ import javafx.collections.ObservableList;
 public class RecipeManager {
   private ObservableList<Recipe> list;
 
-  public RecipeManager() {
-    list = FXCollections.observableArrayList();
+    public RecipeManager() {
+    list = FXCollections.observableArrayList(
+            new Recipe("Hot dog", "Put the hot dog into water and heat until boiling",
+                    10, 400, 10.0, 25.0, 15.0, 1, "Easy"),
+            new Recipe("Pizza", "Flatten out the dough, put the toppings on and heat in a 200°C oven for 12-15min.",
+                    30, 600, 20.0, 40.0, 15.0, 2, "Easy")
+    );;
   }
 
   public ObservableList<Recipe> getList() {
