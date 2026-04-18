f2e59cbf36085f0f7c6a9bc2ff60838ea5e6ae07
BaliVeselaj204 <baliveselaj2@gmail.com>
2026-04-18 11:01:38 +0000

    fix: made the window bigger for images

--- src/main/java/is/vidmot/MainApp.java
+++ src/main/java/is/vidmot/MainApp.java
@@ -15,7 +15,7 @@ import is.vinnsla.RecipeManager;
 public class MainApp extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    Scene scene = new Scene(new Pane(), 900, 700);
+    Scene scene = new Scene(new Pane(), 900, 800);
     ViewSwitcher.setScene(scene);
     ViewSwitcher.switchTo(View.MAIN);
     stage.setScene(scene);
